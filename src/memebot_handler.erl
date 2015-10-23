d3a9ae38f00dccc0f41eba58e2f40ab75272d10a
Tucker <barbct5@gmail.com>
2015-10-23 15:24:30 -0700

    post to slack

--- src/memebot_handler.erl
+++ src/memebot_handler.erl
@@ -1,5 +1,7 @@
 -module(memebot_handler).
 
+-include_lib("erlcloud/include/erlcloud_aws.hrl").
+
 -export([init/3,
 	 handle/2,
 	 terminate/3]).
@@ -25,12 +27,25 @@ handle_method({_, Req}) ->
 
 generate_meme(Opts, Req) ->
     {ok, Meme} = memebot:generate(Opts),
-    Key = lists:flatten(io_lib:format("~s", [[io_lib:format("~2.16.0B",[X]) || <<X:8>> <= erlang:md5(Meme) ]])),
+    Key = lists:flatten(io_lib:format("~s.jpg", [[io_lib:format("~2.16.0B",[X]) || <<X:8>> <= erlang:md5(Meme) ]])),
     erlcloud_s3:put_object(bucket(), Key, Meme, [{acl, public_read}]),
-    GetUrl = erlcloud_s3:make_get_url(86400, bucket(), Key),
-    Body = jsx:encode(#{<<"url">> => list_to_binary(GetUrl)}),
+    Config = erlcloud_aws:default_config(),
+    GetUrl = erlcloud_s3:get_object_url(bucket(), Key, Config#aws_config{s3_scheme = "http://"}),
+    Body = jsx:encode(#{<<"username">> => <<"memebot">>,
+			<<"text">> => <<"">>,
+			<<"icon_emoji">> => <<":ghost:">>,
+		        <<"channel">> => <<"#dave-meme-gen">>,
+		        <<"attachments">> => [
+					      #{<<"image_url">> => list_to_binary(GetUrl)}
+					     ]}),
+    {ok, Result} = post_to_slack(Body),
+    ok = error_logger:info_msg("~p~n", [Result]),
     cowboy_req:reply(200, [{<<"content-type">>, <<"application/json">>}], Body, Req).
 
 bucket() ->
     {ok, Bucket} = application:get_env(memebot, s3_bucket),
     Bucket.
+
+post_to_slack(Body) ->
+    {ok, SlackURL} = application:get_env(memebot, slack_url),
+    httpc:request(post, {SlackURL, [], "application/json", Body}, [], []).
